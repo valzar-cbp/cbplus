d49cbc8aaf51fa258c9c8ea40f449719264e58af
valzar-cbp <valzarmen@zoho.com>
2020-09-11 00:05:21 +0200

    fix cam url, colors, x button

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         cbplus-dev
 // @namespace    https://github.com/valzar-cbp/
-// @downloadURL  https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/index.js
-// @version      1.0.7
+// @downloadURL  https://raw.githubusercontent.com/valzar-cbp/cbplus/master/index.js
+// @version      1.1.0
 // @description  Better Chaturbate!
 // @author       ValzarMen
 // @include      https://www.chaturbate.com/*
 // @include      https://chaturbate.com/*
-// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/cbplus.js
-// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/require/video.min.js
-// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/require/jquery.min.js
-// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/require/jquery-ui.min.js
-// @resource     vjCSS https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/resource/video-js.css
-// @resource     jqCSS https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/resource/jquery-ui.css
-// @resource     cbCSS https://raw.githubusercontent.com/valzar-cbp/cbplus/dev/resource/cbplus.css
+// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/master/cbplus.js
+// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/master/require/video.min.js
+// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/master/require/jquery.min.js
+// @require      https://raw.githubusercontent.com/valzar-cbp/cbplus/master/require/jquery-ui.min.js
+// @resource     vjCSS https://raw.githubusercontent.com/valzar-cbp/cbplus/master/resource/video-js.css
+// @resource     jqCSS https://raw.githubusercontent.com/valzar-cbp/cbplus/master/resource/jquery-ui.css
+// @resource     cbCSS https://raw.githubusercontent.com/valzar-cbp/cbplus/master/resource/cbplus.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @run-at       document-end
